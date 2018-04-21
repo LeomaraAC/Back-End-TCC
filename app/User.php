37b3f8eb81ca2d9240bcd44a88c25b8fae0ccaca
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-04-21 19:42:58 -0300

    Arquivos iniciais do projeto

---
+++ app/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable, SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'prontuario', 'nome', 'email', 'senha',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'senha', 'deleted_at',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function getAuthPassword () {
+
+        return $this->senha;
+
+    }
+
+}
