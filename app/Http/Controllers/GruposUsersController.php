0dfe602cd6d084e14ae2a828cc525713034a897f
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-05-13 20:47:32 -0300

    Criação do controller GrupoUsers

---
+++ app/Http/Controllers/GruposUsersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+/*use App\Telas_Sistema as Telas;
+use App\Grupo_Usuarios as Grupo;*/
+
+class GruposUsersController extends Controller
+{
+    //
+}
