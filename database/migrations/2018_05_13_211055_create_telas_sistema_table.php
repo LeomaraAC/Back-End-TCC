78f058c5883405d11d23f4aef71ff33dad6546b5
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-05-13 20:13:06 -0300

    Criação da migration referente a tabela Telas_Sistema

---
+++ database/migrations/2018_05_13_211055_create_telas_sistema_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelasSistemaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Telas_Sistema', function (Blueprint $table) {
+            $table->increments('idTelas');
+            $table->string("nomeTela",100);
+            $table->string("modulo",100);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Telas_Sistema');
+    }
+}
