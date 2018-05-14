366b11204aeea5856a957c7f7064bd6541479116
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-05-13 23:29:00 -0300

    Criação da função responsável por criar salvar o novo grupo criado, bem como as suas permissões.

--- app/Http/Controllers/GruposUsersController.php
+++ app/Http/Controllers/GruposUsersController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-/*use App\Telas_Sistema as Telas;
-use App\Grupo_Usuarios as Grupo;*/
+use App\Telas_Sistema as Funcoes;
+use App\Grupo_Usuarios as Grupo;
 
 class GruposUsersController extends Controller
 {
-    //
+    public function criarGrupo(Request $request) {
+        $hasError = false;
+        //Gravando o novo grupo
+        $grupo = new Grupo;
+        $grupo->nomeGrupo = $request->nomeGrupo;
+        $grupo->save();
+        // Informando as permssoes do grupo
+        foreach ($request->funcoes as $idFuncao) {
+            $funcao = Funcoes::find($idFuncao);
+            if ($funcao != null)
+                $grupo->funcoes()->attach($funcao->idTelas);
+            else
+                $hasError = true;
+        }
+        if(!$hasError)
+            return response()->json(['message' => 'Grupo criado com sucesso']);
+        else
+            return response()->json(['message'=>'Ops! Ocorreu um erro. O grupo pode não ter sido criado corretamente'],'500');
+    }
 }
