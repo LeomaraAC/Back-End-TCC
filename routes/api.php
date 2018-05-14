01251c7dc8ee093eb6f0178727e83a9bf59f2c38
LEOMARAAC\Leomara <leomara.andrade@gmail.com>
2018-05-14 16:24:54 -0300

    Criação da rota para filtrar os grupos

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,8 @@ Route::group([
     Route::post('grupo', 'GruposUsersController@store');
     Route::get('grupo', 'GruposUsersController@index');
     Route::get('grupo/{id}', 'GruposUsersController@show');
-    Route::delete('grupo', 'GruposUsersController@destroy');
+    Route::delete('grupo/{id}', 'GruposUsersController@destroy');
     Route::put('grupo', 'GruposUsersController@update');
+    Route::post('filtrarGrupo', 'GruposUsersController@filter');
 
 });
